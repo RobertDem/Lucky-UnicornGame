0aab1ef5903899e8339db1d6d544d84ac342ca95
RobertDem <131831444+RobertDem@users.noreply.github.com>
2023-06-06 10:48:17 +1200

    Add files via upload

---
+++ 1OR10.py
@@ -0,0 +1,19 @@
+error = "Please enter an whole number between 1 and 1\n"
+
+valid = False
+while not valid:
+  try:
+      response= int(input("How much would you" 
+                          "like to play with? "))
+    
+      if 0 < response <= 10:
+        print("You have asked to play "
+              "with ${}".format(response))
+    
+      else: 
+        print(error)
+    
+  except ValueError:
+    print(error)
+
+    
